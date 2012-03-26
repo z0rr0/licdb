686f9611de2f5e49aac25779824cdd1922ab787e
Alexander Zaycev APINGTU <zaycev@apingtu.edu.ru>
2012-03-26 16:12:16 +0400

    add error template

--- main/main/urls.py
+++ main/main/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf import settings
+from django.contrib.auth.views import login, logout
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'main.views.home', name='home'),
@@ -15,3 +18,19 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# media content                   
+# urlpatterns += patterns('',) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^robots.txt$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'path': "robots.txt"}),
+        url(r'^favicon.ico$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'path': "favicon.ico"}),
+   	)
+    # urlpatterns += patterns('django.contrib.staticfiles.views',
+    #     url(r'^static/(?P<path>.*)$', 'serve'),
+    # )
