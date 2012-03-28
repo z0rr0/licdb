519ca9f018d498cd8560deced23ff145eecf1ab7
Alexander Zaycev APINGTU <zaycev@apingtu.edu.ru>
2012-03-28 12:46:06 +0400

    first models key

--- main/key/admin.py
+++ main/key/admin.py
@@ -1,3 +1,17 @@
 #-*- coding: utf-8 -*-
 from django.contrib import admin
+from key.models import License, Program
 
+class LicenseAdmin(admin.ModelAdmin):
+	search_fields = ('name',)
+
+class ProgramAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'license')
+	# search_fields = ('name', 'license__name')
+	search_fields = ('name',)
+	list_filter = ('license',)
+
+
+
+admin.site.register(License, LicenseAdmin)
+admin.site.register(Program, ProgramAdmin)
--- main/key/models.py
+++ main/key/models.py
@@ -1,49 +1,80 @@
 #-*- coding: utf-8 -*-
 from django.db import models
 
-class Program (models.Model):
-    u"""
-    Название программы
-    
-    """
-    name = models.CharField(max_length = 100, verbose_name = u'Название программы')
-    use_student = models.BooleanField(verbose_name = u'Выдача студентам',)
-
 class License (models.Model):
-    u"""
-    Лицензии на имеющиеся программы
-    
-    """
-    program = models.ForeignKey(Program, verbose_name = u'Название программы')
-    date_start = models.DateField(verbose_name = u'Дата получения лицензии',)
-    date_end = models.DateField(null = True, verbose_name = u'Дата окончания лицензии')
-
-class Tip (models.Model):
     u"""
     Тип лицензии
     
     """
-    comment = models.TextField(blank = True, verbose_name = u'Комментарий')
+    name = models.CharField(max_length = 127, verbose_name = u'название')
+    attach = models.FileField(verbose_name = u'файл', blank = True, null = True, upload_to = 'license')
+    url = models.URLField(max_length = 512, verbose_name = u'сайт', blank = True, null = True, help_text = u'адрес сайта')
+    comment = models.TextField(verbose_name = u'комментарий', blank = True, null = True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Program (models.Model):
+    u"""
+    Программное обеспечение
+    
+    """
+    license = models.ForeignKey(License, verbose_name = u'лицензия')
+    name = models.CharField(max_length = 255, verbose_name = u'название')
+    use_student = models.BooleanField(verbose_name = u'выдача студентам', default = False, help_text=u'можно ли студентам использовать данное ПО')
+    url = models.URLField(max_length = 512, verbose_name = u'сайт', blank = True, null = True, help_text = u'адрес сайта программы')
+    comment = models.TextField(verbose_name = u'примечание', blank = True, null = True)
+    # даты изменения и создания, заполняются автоматически
+    modified = models.DateTimeField(auto_now = True, auto_now_add = True, editable = False)
+    created = models.DateTimeField(auto_now_add = True, editable = False)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
     
 class Key (models.Model):
     u"""
     Ключи и файлы лицензий
     
     """
-    license = models.ForeignKey(License, verbose_name = u'Лицензия')
-    key = models.CharField(max_length = 255, blank = True, unique = 'True', verbose_name = u'Ключ')
-    use = models.BooleanField(verbose_name = u'Изпользуется',)
-    tip = models.ForeignKey(Tip, verbose_name = u'Тип')
-    comment = models.CharField(max_length = 500, blank = True, verbose_name = u'Комментарий')
+    program = models.ForeignKey(Program, verbose_name = u'программа')
+    key = models.CharField(max_length = 255, blank = True, unique = 'True', verbose_name = u'ключ')
+    attach = models.FileField(verbose_name = u'файл', blank = True, null = True, upload_to = 'keys')
+    use = models.BooleanField(verbose_name = u'используется', default = False)
+    date_start = models.DateField(verbose_name = u'начало', help_text = u'дата получения лицензии')
+    date_end = models.DateField(verbose_name = u'окончание', blank = True, null = True, help_text = u'Дата окончания лицензии')
+    comment = models.TextField(verbose_name = u'комментарий', blank = True, null = True)
+    # дата создания
+    created = models.DateTimeField(auto_now_add = True, editable = False)
+
+    def __unicode__(self):
+        return "%s %s" % (self.program.name, self.key)
+
+    class Meta:
+        ordering = ['use', 'program__name', 'key']
     
 class Client (models.Model):
     u"""
-    Клиенты получившие лицензию
+    Пользователи (люди или отделы), получившие лицензию
     
     """
-    name = models.CharField(max_length = 300, verbose_name = u'Имя')
-    gruppa = models.CharField(max_length = 30, blank = True, verbose_name = u'Группа')
-    date_start = models.DateField(verbose_name = u'Дата выдачи',)
-    key = models.ForeignKey(Key, verbose_name = u'Выданный ключ')
-    comment = models.CharField(max_length = 500, blank = True, verbose_name = u'Комментарий')
+    name = models.CharField(max_length = 300, verbose_name = u'имя', help_text = u'даные пользователя, получившего лицензию')
+    student = models.BooleanField(verbose_name = u'студент', default = False, help_text = u'используется студентом')
+    date_start = models.DateField(verbose_name = u'дата выдачи',)
+    key = models.ForeignKey(Key, verbose_name = u'ключ')
+    comment = models.TextField(verbose_name = u'комментарий', blank = True, null = True)
+    # даты изменения и создания, заполняются автоматически
+    modified = models.DateTimeField(auto_now = True, auto_now_add = True, editable = False)
+    created = models.DateTimeField(auto_now_add = True, editable = False)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name', 'key__program__name']
     
