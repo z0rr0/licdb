72bb19df1a22aa496ea93a88a87b5a7398c0e728
Alexander Zaycev APINGTU <zaycev@apingtu.edu.ru>
2012-04-03 12:45:07 +0400

    add tiny_mce and ckeditor

---
+++ main/main/media/ckeditor/config.js
@@ -0,0 +1,27 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	config.language = 'ru';
+	config.uiColor = '#EEEEEE';
+	// config.toolbar = 'Basic';
+	config.toolbar = 'MyToolbar';
+	config.toolbar_MyToolbar =
+	[
+		//{ name: 'document', items : [ 'NewPage','Preview' ] },
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+		{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','SpecialChar' ] },
+		{ name: 'tools', items : [ 'Maximize','-','About' ] },
+                '/',
+		{ name: 'styles', items : [ 'Styles','Format' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] }
+	];
+};
+
